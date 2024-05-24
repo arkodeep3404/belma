9e3f18cea490aeb1e99e5752c76fd706c5f2ae54
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-05-24 15:21:03 +0530

    auth working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthContextProvider } from "./context/auth";
+import StateContext from "./context/stateContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthContextProvider>
+    <StateContext>
+      <App />
+    </StateContext>
+  </AuthContextProvider>
+);
